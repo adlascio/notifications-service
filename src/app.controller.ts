bd3c10feea14a6a032eefc09d1ff80db876f985e
Arthur Di Lascio <31333091+adlascio@users.noreply.github.com>
2022-12-13 11:10:17 -0800

    add post request to create new notification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'node:crypto';
 import { PrismaService } from './prisma.service';
 
 @Controller('notifications')
@@ -6,7 +7,21 @@ export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getHello() {
+  list() {
     return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: any) {
+    console.log('body', body);
+    const { content, category, recipientId } = body;
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
   }
 }
