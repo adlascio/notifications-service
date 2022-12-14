b4104e1fdd20fcb15492d5a9c8df60d3f12e991a
Arthur Di Lascio <31333091+adlascio@users.noreply.github.com>
2022-12-14 09:57:31 -0800

    add notification repository with test

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,15 +1,26 @@
 import { SendNotification } from './send-notification';
+import { Notification } from '../entities/notification';
+
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       recipientId: 'example-recipient-id',
       category: 'notification_test',
       content: 'New notification',
     });
 
-    expect(notification).toBeTruthy();
+    console.log(notifications);
+
+    expect(notifications).toHaveLength(1);
   });
 });
